d736b46dc79910e223883a5093ead5310a1f3c29
万聂青 <919624032@qq.com>
2024-02-20 22:07:16 +0800

    wip

--- apiserver/spec.go
+++ apiserver/spec.go
@@ -1,7 +1,7 @@
 package apiserver
 
 const (
-	CheckLogin            = "/api/check-IsLogin"
+	CheckLogin            = "/api/check-login"
 	GetUserInfo           = "/api/userinfo"
 	SendText              = "/api/send-text"
 	GetContactList        = "/api/contact-list"
